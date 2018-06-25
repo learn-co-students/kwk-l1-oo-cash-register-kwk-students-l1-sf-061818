793af4c0b28bb6523abd9047fb71592226a40de6
Mariana <mantaya00@gmail.com>
2018-06-25 16:04:50 +0000

    Done.

--- amazon_transaction.rb
+++ amazon_transaction.rb
@@ -1,1 +1,45 @@
 # Code your cash register here!
+class AmazonTransaction 
+  def initialize (discount = 0, items = [], total = 0)
+    @total = total
+    @items = items 
+    @discount = discount 
+  end 
+  
+  def add_item (title, price, quantity=1)
+    @total += price*quantity
+
+    while quantity != 0
+        @items << title
+        quantity -= 1
+      end
+
+    
+    
+  end
+    
+  def total=(total)
+    @total = total
+  end
+  
+  def total
+    @total
+  end
+  def items 
+    @items 
+  end 
+  
+  def apply_discount
+    newcount=@discount/100.00
+    x = newcount*@total
+    @total -= x
+    if @discount>0
+      return "After the discount, the total comes to $#{@total.to_i}."
+    else
+      return "There is no discount to apply."
+    end
+  end
+    
+end 
+
+
